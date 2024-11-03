3259288d928c752888b99db6d1b162f834592fef
SaifAli111333 <saif64459@gmail.com>
2024-11-03 11:35:44 -0800

    11/4.2

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-const http = require('http')
-const app =require('./app')
-const server=http.createServer(app);
-server.listen(80,console.log('server running on localhost 80'));
+// const http = require('http')
+// const app =require('./app')
+// const server=http.createServer(app);
+// server.listen(80,console.log('server running on localhost 80'));
+const http = require('http');
+const app = require('./app');
+
+const server = http.createServer(app);
+
+// Listen on port 80 and log a message once the server is ready
+server.listen(80, '0.0.0.0', () => {
+    console.log('Server running on http://0.0.0.0:80');
+});
