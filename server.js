b02d16261a24af0c7122fe1755926f46afbc4cc7
SaifAli111333 <saif64459@gmail.com>
2024-11-03 12:11:14 -0800

    11/4.4

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const app = require('./app');
 const server = http.createServer(app);
 
 // Listen on port 80 and log a message once the server is ready
-server.listen(80, '0.0.0.0', () => {
+server.listen(3000, '0.0.0.0', () => {
     console.log('Server running on http://0.0.0.0:80');
 });
